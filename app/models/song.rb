dfa18a641a516db8be0f316c0f6fb768a4c414fa
Matt Beaudoin <ehxtwo@gmail.com>
2021-05-03 08:28:34 -0700

    commit tree bc4ba4e3d09ef235e42e8c324b9a75f75ae29c25

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,29 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+  end
+
+  def artist_name=(artist)
+    self.artist = Artist.find_or_create_by(name: artist)
+    self.save
+  end
+
+  def note_contents
+    self.notes.collect(&:content)
+  end
+
+  def note_contents=(note_contents)
+    note_contents.each do |text|
+      unless text.empty?
+        self.notes << Note.create(content: text)
+        self.save
+      end
+    end
+  end
 end
